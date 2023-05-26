a890554b951a20df4e3e511a079495d92e6e53e5
Devvrat Singh Rathod <73027078+Devvrat1010@users.noreply.github.com>
2023-05-26 23:59:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,19 +9,16 @@ let arr=["<img src='https://i.ibb.co/x64L479/dice1.png'>",
         "<img src='https://i.ibb.co/QbxpsDJ/dice5.png'>" ,
         "<img src='https://i.ibb.co/mt7HSnY/dice6.png'>" 
 ];
+
 let r=2;
-console.log(arr[r])
 
 let img1="'images/dice"+r1+".png'";
 let img1_final="<img src="+img1+" alt=''>";
 
-console.log(img1);
 
 let img2="'images/dice"+r2+".png'";
 let img2_final="<img src="+img2+" alt=''>";
-console.log(img2);
-// let check = document.querySelector(".dice1");
-// console.log(check);
+
 document.querySelector(".dice1").innerHTML = arr[r1];
 document.querySelector(".dice2").innerHTML = arr[r2];
 
